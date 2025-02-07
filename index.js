0eb18fa79267aab1f2bd710f2a6af798699e6985
sasha <sasha.friis@icloud.com>
2025-02-07 22:00:39 +0100

    grund layout + plus alt date funktionel

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 fetch("/data/destinations.json")
     .then(response => response.json())
     .then(data => {
-        let sectionElm = document.createElement("section")
-        sectionElm.innerHTML = data.destinations.map(destination => `
-            <div class="apartment-container">
-                <article>
-                    <picture>
+        let sectionElm = document.createElement("section");
+        sectionElm.classList.add("apartment-listings");
+
+        let headingElm = document.createElement("h1");
+        headingElm.textContent = "Apartments for rent";  
+
+        sectionElm.innerHTML = `
+            ${data.destinations.map(destination => `
+                    <article>
                         <img src="/img/${destination.image}" alt="${destination.destination}">
-                    <div class="apartment-listing">
-                        <p style="text-align:left;" class="apartment-listing-txt">
-                            &#9829;
-                            <a href="destination.html?id=${destination.id}">
-                                more
-                            </a>
-                        </p>
-                    </div>
-                </article>
-            </div>`).join("")
-        document.querySelector("#root").append(sectionElm)
+                        <div class="apartment-listing">
+                            <p style="text-align:left;" class="apartment-listing-txt">
+                                &#9829;
+                                <a href="destination.html?id=${destination.id}" style="float: right;" class="apartment-listing-txt">MORE</a>
+                            </p>
+                        </div>
+                    </article>
+            `).join("")}
+        `;
+
+        document.querySelector("#root").append(sectionElm);
+        document.querySelector("#title").append(headingElm)
     })
